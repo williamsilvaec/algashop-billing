450a1753e33c82c78f26d3df5b6ddb5b61db2c57
William Silva <williamsilvaec@gmail.com>
2026-01-23 15:36:03 -0300

    refactor: refatorações gerais em todos os microserviços.

--- src/main/java/com/williamsilva/algashop/billing/BillingApplication.java
+++ src/main/java/com/williamsilva/algashop/billing/BillingApplication.java
@@ -3,10 +3,13 @@ package com.williamsilva.algashop.billing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BillingApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(BillingApplication.class, args);
 	}
 
